6a15312c4c57e54fa7160ac218c49d2236a7056d
Per Thulin <per@youtify.com>
2014-01-21 14:58:28 +0100

    Use dropdown to navigate pages

--- js/directives/mainNav.js
+++ js/directives/mainNav.js
@@ -6,13 +6,40 @@ function MainNav($location, api) {
     },
     templateUrl: '/html/directives/mainNav.html',
     link: function (scope, element) {
+
+      function getPageFromUrl() {
+        var split = $location.path().split('/');
+
+        if (split[1] !== "pages") {
+          return null;
+        }
+
+        return split[2];
+      }
+
+      scope.isOnWelcomeScreen = function() {
+        return $location.path() === '/pages';
+      };
+
+      scope.openSelectedPage = function () {
+        $location.path('/pages/' + scope.selectedPage + '/posts');
+      };
+
       scope.isCurrentPage = function (id) {
         var split = $location.path().split('/');
-        return id === split[1];
+        return id === getPageFromUrl();
       };
 
       api.getUser().success(function(data) {
         scope.user = data;
+      });
+
+      api.getPages().success(function(data) {
+        scope.pages = data;
+        scope.selectedPage = getPageFromUrl();
+        if (!scope.selectedPage && scope.pages.length > 0) {
+          scope.selectedPage = scope.pages[0].id;
+        }
       });
     }
   };
