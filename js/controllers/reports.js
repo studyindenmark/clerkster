7086dcfb23d827302ae2d93550fffa7b2a3d8b99
Per Thulin <per@youtify.com>
2014-01-22 11:46:05 +0100

    Show user's email in reports section

--- js/controllers/reports.js
+++ js/controllers/reports.js
@@ -1,3 +1,8 @@
 function ReportsController($scope, $routeParams, api) {
     $scope.pageId = $routeParams.page_id;
+    $scope.user = {};
+
+    api.getUser().success(function(data) {
+      $scope.user = data;
+    });
 }
