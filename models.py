fb0ddc66260911029e51c5c6ddc96b3fa73f710f
Per Thulin <per@youtify.com>
2014-01-22 10:45:43 +0100

    Fetch comments from posts as well

--- models.py
+++ models.py
@@ -31,6 +31,9 @@ class FacebookPost(ndb.Model):
       'message': self.message,
       'created_time': self.created_time.strftime('%s'),
       'updated_time': self.updated_time.strftime('%s'),
+      'comments': [m.json for m in FacebookComment.\
+        query(ancestor=self.key).\
+        order(-FacebookComment.created_time)],
       'from': {
         'id': self.from_id,
         'name': self.from_name,
@@ -42,17 +45,20 @@ class FacebookPost(ndb.Model):
 
 class FacebookComment(ndb.Model):
   message = ndb.StringProperty()
-  date = ndb.DateTimeProperty()
-  facebook_user_id = ndb.StringProperty()
-  facebook_user_name = ndb.StringProperty()
-  scanned_user_name = ndb.StringProperty()
+  created_time = ndb.DateTimeProperty()
+  from_id = ndb.StringProperty()
+  from_name = ndb.StringProperty()
+  from_category = ndb.StringProperty()
 
   @property
   def json(self):
     return {
       'id': self.key.id(),
-      'date': self.date.strftime('%s'),
-      'facebook_user_id': self.facebook_user_id,
-      'facebook_user_name': self.facebook_user_name,
-      'scanned_user_name': self.scanned_user_name,
+      'message': self.message,
+      'created_time': self.created_time.strftime('%s'),
+      'from': {
+        'id': self.from_id,
+        'name': self.from_name,
+        'category': self.from_category,
+      },
     }
--- worker.py
+++ worker.py
@@ -28,11 +28,28 @@ class WorkerHandler(RequestHandler):
         parent=page.key,
         id=post.get('id'),
         created_time=FacebookAPI.parse_date(post.get('created_time')),
-        updated_time=FacebookAPI.parse_date(post.get('created_time')),
+        updated_time=FacebookAPI.parse_date(post.get('updated_time')),
         type=post.get('type'),
         from_id=post.get('from').get('id'),
         from_name=post.get('from').get('name'),
         from_category=post.get('from').get('category'),
         message=post.get('message') or post.get('story'),
+      )
+      m.put()
+      self.create_comments(post, m)
+
+  def create_comments(self, post_data, post_model):
+    if not 'comments' in post_data:
+      return
+
+    for comment in post_data.get('comments').get('data'):
+      m = FacebookComment(
+        parent=post_model.key,
+        id=comment.get('id'),
+        created_time=FacebookAPI.parse_date(comment.get('created_time')),
+        from_id=comment.get('from').get('id'),
+        from_name=comment.get('from').get('name'),
+        from_category=comment.get('from').get('category'),
+        message=comment.get('message'),
       )
       m.put()
