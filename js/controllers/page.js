1979b267f5e26863c380f1676746176307102573
Per Thulin <per@youtify.com>
2014-01-27 11:19:51 +0100

    Make it possible to search by creation_time

--- js/controllers/page.js
+++ js/controllers/page.js
@@ -12,16 +12,35 @@ function PageController($scope, $routeParams, $q, api) {
   });
 
   $scope.search = function() {
-    var q = '';
+    var q =[];
 
     if ($scope.author) {
-      q = 'author:' + $scope.author;
+      q.push('author:' + $scope.author);
     }
-    
-    api.search($scope.pageId, q).success(function(data) {
-      $scope.posts = data;
-      $scope.empty = $scope.posts.length === 0;
-    });
+
+    if ($scope.from) {
+      q.push('created_time > ' + $scope.from);
+    }
+
+    if ($scope.to) {
+      q.push('created_time < ' + $scope.to);
+    }
+
+    q = q.join(' AND ');
+
+    console.log(q);
+
+    if (q) {
+      api.search($scope.pageId, q).success(function(data) {
+        $scope.posts = data;
+        $scope.empty = $scope.posts.length === 0;
+      });
+    } else {
+      api.getPosts($scope.pageId).success(function(data) {
+        $scope.posts = data;
+        $scope.empty = $scope.posts.length === 0;
+      });
+    }
   };
 }
 
