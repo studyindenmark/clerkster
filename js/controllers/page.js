33ac8c2691d21482844ca9f03a6bbb0d94ca099e
Per Thulin <per@youtify.com>
2014-01-27 12:00:22 +0100

    Update URL with search components

--- js/controllers/page.js
+++ js/controllers/page.js
@@ -1,46 +1,45 @@
-function PageController($scope, $routeParams, $q, api) {
+function PageController($scope, $location, $routeParams, $q, api) {
   $scope.pageId = $routeParams.page_id;
-  $scope.author = '';
+  $scope.author = $routeParams.author || '';
+  $scope.from = $routeParams.from || null;
+  $scope.to = $routeParams.to || null;
 
   api.getPage($scope.pageId).success(function(data) {
     $scope.page = data;
   });
 
-  api.getPosts($scope.pageId).success(function(data) {
-    $scope.posts = data;
-    $scope.empty = $scope.posts.length === 0;
-  });
+  var q =[];
 
-  $scope.search = function() {
-    var q =[];
+  if ($scope.author) {
+    q.push('author:' + $scope.author);
+  }
 
-    if ($scope.author) {
-      q.push('author:' + $scope.author);
-    }
+  if ($scope.from) {
+    q.push('created_time > ' + $scope.from);
+  }
 
-    if ($scope.from) {
-      q.push('created_time > ' + $scope.from);
-    }
+  if ($scope.to) {
+    q.push('created_time < ' + $scope.to);
+  }
 
-    if ($scope.to) {
-      q.push('created_time < ' + $scope.to);
-    }
+  q = q.join(' AND ');
 
-    q = q.join(' AND ');
+  if (q) {
+    api.search($scope.pageId, q).success(function(data) {
+      $scope.posts = data;
+      $scope.empty = $scope.posts.length === 0;
+    });
+  } else {
+    api.getPosts($scope.pageId).success(function(data) {
+      $scope.posts = data;
+      $scope.empty = $scope.posts.length === 0;
+    });
+  }
 
-    console.log(q);
-
-    if (q) {
-      api.search($scope.pageId, q).success(function(data) {
-        $scope.posts = data;
-        $scope.empty = $scope.posts.length === 0;
-      });
-    } else {
-      api.getPosts($scope.pageId).success(function(data) {
-        $scope.posts = data;
-        $scope.empty = $scope.posts.length === 0;
-      });
-    }
+  $scope.update = function() {
+    $location.search('author', $scope.author || null);
+    $location.search('from', $scope.from || null);
+    $location.search('to', $scope.to || null);
   };
 }
 
