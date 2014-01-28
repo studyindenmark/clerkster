4e1e82f26a9edae0c8b0b363221016fefcb53d2f
Per Thulin <per@youtify.com>
2014-01-28 12:47:16 +0100

    Use different queues for mail and facebook

--- cron.py
+++ cron.py
@@ -8,23 +8,27 @@ from webapp2_extras.appengine.auth.models import User
 class CronHandler(RequestHandler):
 
   def fetch_pages(self):
+    queue = taskqueue.Queue('facebook')
     keys = Page.query().fetch(keys_only=True)
 
     for key in keys:
-      taskqueue.add(
+      task = taskqueue.Task(
         url='/worker/fetch_page',
         params={
           'key': key.urlsafe(),
         }
       )
+      queue.add(task)
 
   def send_reports(self):
+    queue = taskqueue.Queue('mail')
     keys = User.query().fetch(keys_only=True)
 
     for key in keys:
-      taskqueue.add(
+      task = taskqueue.Task(
         url='/worker/send_report',
         params={
           'key': key.urlsafe(),
         }
       )
+      queue.add(task)
